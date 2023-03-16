bccea54bacac7aeebcc80461dc0026a23e7c3cae
Junior Souza <jrsouzasolucoes@gmail.com>
2023-03-16 15:00:20 -0300

    Adicionar arquivos de projeto.

---
+++ DesafioDiamante.ConsoleApp/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+
+namespace DesafioDiamante.ConsoleApp;
+class Program
+{
+    static void Main(string[] args)
+    {
+        int numeroImpar = 21;
+        string caractere = "X";
+
+        for (int i = 1; i <= numeroImpar; i += 2)
+        {
+            int espacoEmBranco = (numeroImpar - i) / 2;
+            for (int j = 0; j < espacoEmBranco; j++)
+            {
+                Console.Write(" ");
+            }
+            for (int j = 0; j < i; j++)
+            {
+                Console.Write(caractere);
+            }
+            Console.WriteLine();
+        }
+
+        for (int i = numeroImpar - 2; i >= 1; i -= 2)
+        {
+            int espacoEmBranco = (numeroImpar - i) / 2;
+            for (int j = 0; j < espacoEmBranco; j++)
+            {
+                Console.Write(" ");
+            }
+            for (int j = 0; j < i; j++)
+            {
+                Console.Write(caractere);
+            }
+            Console.WriteLine();
+        }
+
+        Console.ReadKey();
+    }
+}
